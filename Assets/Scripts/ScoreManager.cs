4c6b70aef78667a41571102af8147aba5f1129b1
brendarendonnavis <brenda.rendon98@gmail.com>
2017-10-28 13:15:43 -0700

    WORKING Candy collector

---
+++ Assets/Scripts/ScoreManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreManager : MonoBehaviour {
+
+	public static int score= 0;
+	Text scoreThing;
+	// Use this for initialization
+	void Start () {
+		scoreThing = GetComponent<Text> ();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		scoreThing.text = string.Format ("Score: {0}", score);
+	}
+}
