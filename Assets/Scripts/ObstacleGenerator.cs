daa04c8e13756eea1c211bb2bf8e1ab539da6a8b
tworii <tworii@users.noreply.github.com>
2017-10-28 12:39:44 -0700

    Added Basic Obstacle Spawning
    Still Bugged
    -Objects that spawn do not render in the play view environment

---
+++ Assets/Scripts/ObstacleGenerator.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleGenerator : MonoBehaviour {
+
+    public GameObject theObstacle;
+    public Transform generationPoint;
+    public float distanceBetween;
+
+    public float distanceBetweenMin;
+    public float distanceBetweenMax;
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (transform.position.x < generationPoint.position.x)
+        {
+
+            distanceBetween = Random.Range(distanceBetweenMin, distanceBetweenMax);
+
+            transform.position = new Vector3(transform.position.x + distanceBetween,
+                transform.position.y, transform.position.z);
+
+            Instantiate(theObstacle, transform.position, transform.rotation);
+        }
+
+    }
+}
