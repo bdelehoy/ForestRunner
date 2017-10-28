9be9c98f9c09c1540fb02795876d00a2465a38c8
tworii <tworii@users.noreply.github.com>
2017-10-28 11:14:34 -0700

    Fixed Basic Platform Spawning/Despawning

--- Assets/Scripts/PlatformGenerator.cs
+++ Assets/Scripts/PlatformGenerator.cs
@@ -21,7 +21,7 @@ public class PlatformGenerator : MonoBehaviour {
 
         if(transform.position.x < generationPoint.position.x)
         {
-            transform.position = new Vector3(transform.position.x + distanceBetween,
+            transform.position = new Vector3(transform.position.x + platformWidth + distanceBetween,
                 transform.position.y, transform.position.z);
 
             Instantiate(thePlatform, transform.position, transform.rotation);
