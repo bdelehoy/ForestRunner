df653c630ca97988a58b00e0477cd8ae194f1645
tworii <tworii@users.noreply.github.com>
2017-10-27 21:47:25 -0700

    Added Basic Movement/Control

---
+++ Assets/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    public float jumpForce;
+
+    private Rigidbody2D playerRB;
+
+
+    public bool grounded;
+    public LayerMask whatIsFloor;
+
+    private Collider2D playerCollider;
+
+	// Use this for initialization
+	void Start () {
+        playerRB = GetComponent<Rigidbody2D>();
+
+        playerCollider = GetComponent<Collider2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        grounded = Physics2D.IsTouchingLayers(playerCollider, whatIsFloor);
+
+        playerRB.velocity = new Vector2(moveSpeed, playerRB.velocity.y);
+
+        if((Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0)) && grounded == true)
+        {
+            playerRB.velocity = new Vector2(playerRB.velocity.x, jumpForce);
+        }
+	}
+}
