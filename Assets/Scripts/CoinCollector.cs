986befcf657f791fbd7181b29739743afe1c0aa8
brendarendonnavis <brenda.rendon98@gmail.com>
2017-10-28 12:58:21 -0700

    Revert "Updated working candy collector"
    This reverts commit 09c15804aca2805c2400829e0b5fb623c7bc7bab.

--- Assets/Scripts/CoinCollector.cs
+++ Assets/Scripts/CoinCollector.cs
@@ -17,5 +17,4 @@ public class CoinCollector : MonoBehaviour {
 				ScoreManager.score++;
 				Destroy (gameObject);
 			}
-	}
 }
--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -2,20 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CandyScript : MonoBehaviour {
+public class ScoreManager : MonoBehaviour {
 
+	public static int score= 0;
+	Text scoreThing;
 	// Use this for initialization
 	void Start () {
-		
+		scoreThing = GetComponent<Text> ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	}
-	void OnTriggerEnter2D(Collider2D other){
-		if (other.tag == "Player") {
-			ScoreManager.score++;
-			Destroy (gameObject);
-		}
+		scoreThing.text = string.Format ("Score: {0}", score);
 	}
 }
