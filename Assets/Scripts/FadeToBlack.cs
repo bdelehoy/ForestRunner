b7d19fdee7e9518520e1c8ed7033d20f14427397
JasonMehroziad <33169117+JasonMehroziad@users.noreply.github.com>
2017-10-28 18:20:12 -0700

    Added button sounds and menu fade

---
+++ Assets/Scripts/FadeToBlack.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class FadeToBlack : MonoBehaviour {
+
+    Image black;
+
+    void Start()
+    {
+        black = GetComponent<Image>();
+    }
+
+    public void FadeOut()
+    {
+        StartCoroutine(Fade());
+    }
+
+    public IEnumerator Fade()
+    {
+
+        for (float i = 0; i <= 1; i += 0.01f)
+        {
+            black.color = new Color(1, 1, 1, i);
+            yield return new WaitForSeconds(Time.deltaTime);
+
+        }
+    }
+
+}
