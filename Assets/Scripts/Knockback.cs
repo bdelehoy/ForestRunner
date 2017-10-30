d304fd59ed656ed368c12a63738cff50fce46926
JasonMehroziad <33169117+JasonMehroziad@users.noreply.github.com>
2017-10-29 18:21:54 -0700

    Revert "Revert "added killzone + sound. again.""
    This reverts commit b458548da91b0d1ad4eac4014524f7d52489f9db.

---
+++ Assets/Scripts/Knockback.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knockback : MonoBehaviour {
+
+    private void OnCollisionEnter2D(Collider2D other)
+    {
+        if (other.tag == "Player")
+        {
+            GetComponent<AudioSource>().Play();
+        }
+    }
+}
