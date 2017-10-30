fa40b309780d096f79a6f0c411bfe3f8eaaa11d8
brendarendonnavis <brenda.rendon98@gmail.com>
2017-10-29 21:31:20 -0700

    Scripts for Lives

--- Assets/Scripts/LivesManager.cs
+++ Assets/Scripts/LivesManager.cs
@@ -3,14 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class HealthManager : MonoBehaviour {
+public class LivesManager : MonoBehaviour {
+
 	public static int lives= 3;
 	Text livesLeft;
+
 	// Use this for initialization
 	void Start () {
 		livesLeft = GetComponent<Text> ();
 	}
-	
+
 	// Update is called once per frame
 	void Update () {
 		livesLeft.text = string.Format ("x {0}", lives);
